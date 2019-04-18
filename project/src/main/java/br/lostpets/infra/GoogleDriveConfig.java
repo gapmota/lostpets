dd71004066e7a65a0841c4d653288dfe744bac6a
natanael.lima <natanael.lima@tivit.com>
2019-04-18 14:53:55 -0300

    1.1.0.0 | Class GoogleDriveConfig

---
+++ project/src/main/java/br/lostpets/infra/GoogleDriveConfig.java
@@ -0,0 +1,87 @@
+package br.lostpets.infra;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class GoogleDriveConfig {
+
+	private static final String APPLICATION_NAME = "ProjectLostPets";
+	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static final String TOKENS_DIRECTORY_PATH = "tokens";
+	private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
+	private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+		// Load client secrets.
+		InputStream in = GoogleDriveConfig.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+		// Build flow and trigger user authorization request.
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+				clientSecrets, SCOPES)
+						.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+						.setAccessType("offline").build();
+		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+	}
+
+	public static void UploadFile(/* String path */Drive service) throws IOException {
+		File fileMetadata = new File();
+		fileMetadata.setName("Koala.jpg");
+		fileMetadata.setMimeType("image/jpeg");
+		// service.files().
+		java.io.File filePath = new java.io.File("Koala.jpg");
+		FileContent mediaContent = new FileContent("image/jpeg", filePath);
+		File file = service.files().create(fileMetadata, mediaContent).setFields("id").execute();
+		System.out.println("File ID: " + file.getId());
+	}
+
+	public static void listFiles(Drive service) throws IOException {
+		// Print the names and IDs for up to 10 files.
+		FileList result = service.files().list().setPageSize(10).setFields("nextPageToken, files(id, name)").execute();
+		List<File> files = result.getFiles();
+		if (files == null || files.isEmpty()) {
+			System.out.println("No files found.");
+		} else {
+			System.out.println("Files:");
+			for (File file : files) {
+				System.out.printf("%s (%s)\n", file.getName(), file.getId());
+			}
+		}
+
+	}
+
+	public static Drive getService() throws GeneralSecurityException, IOException {
+		// Build a new authorized API client service.
+		Drive service;
+		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+		
+		return service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+				.setApplicationName(APPLICATION_NAME).build();
+	}
+
+	public static void main(String... args) throws IOException, GeneralSecurityException {
+		
+		listFiles(getService());
+	}
+}
