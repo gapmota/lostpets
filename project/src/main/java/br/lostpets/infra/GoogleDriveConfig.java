01358afe44c91b901e08e36787a653ff9123c596
henrique-gs <henriquegodez@gmail.com>
2019-05-02 20:36:43 -0300

    V1.2.0.1 | Deleted some unnecessary lines

--- project/src/main/java/br/lostpets/infra/GoogleDriveConfig.java
+++ project/src/main/java/br/lostpets/infra/GoogleDriveConfig.java
@@ -6,25 +6,19 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.FileContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
 
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
-
-import javax.swing.JFileChooser;
 
 public class GoogleDriveConfig {
 
@@ -33,7 +27,6 @@ public class GoogleDriveConfig {
 	private static final String TOKENS_DIRECTORY_PATH = "tokens";
 	private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
 	private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-	private static BufferedImage imagem;
 
 	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
 		// Load client secrets.
@@ -49,31 +42,6 @@ public class GoogleDriveConfig {
 		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
 	}
 
-	public static void UploadFile(java.io.File arq, Drive service) throws IOException {
-		File fileMetadata = new File();
-		fileMetadata.setName(arq.getName());
-		fileMetadata.setMimeType("image/jpeg");
-		java.io.File filePath = new java.io.File(arq.getAbsolutePath());
-		FileContent mediaContent = new FileContent("image/jpeg", filePath);
-		File file = service.files().create(fileMetadata, mediaContent).setFields("id").execute();
-		System.out.println("File ID: " + file.getId());
-	}
-
-	public static void listFiles(Drive service) throws IOException {
-		// Print the names and IDs for up to 10 files.
-		FileList result = service.files().list().setPageSize(10).setFields("nextPageToken, files(id, name)").execute();
-		List<File> files = result.getFiles();
-		if (files == null || files.isEmpty()) {
-			System.out.println("No files found.");
-		} else {
-			System.out.println("Files:");
-			for (File file : files) {
-				System.out.printf("%s (%s)\n", file.getName(), file.getId());
-			}
-		}
-
-	}
-
 	public static Drive getService() throws GeneralSecurityException, IOException {
 		// Build a new authorized API client service.
 		Drive service;
@@ -81,19 +49,5 @@ public class GoogleDriveConfig {
 		
 		return service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
 				.setApplicationName(APPLICATION_NAME).build();
-	}
-
-	public static void main(String... args) throws IOException, GeneralSecurityException {
-		
-		JFileChooser fc = new JFileChooser();
-        int res = fc.showOpenDialog(null);
-
-        if (res == JFileChooser.APPROVE_OPTION) {
-            java.io.File arquivo = fc.getSelectedFile();
-            UploadFile(arquivo, getService());
-        } else 
-        	System.out.println("Voce nao selecionou nenhum arquivo.");
-		
-		listFiles(getService());
 	}
 }
