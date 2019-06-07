9868bc64240432cf2f5f76cfee471da4ab25d0e6
Juvencio Jose <juvencio-ramalho@hotmail.com>
2019-06-06 22:03:00 -0300

    mudança em controller de login e pagina html

--- project/src/main/java/br/lostpets/project/controller/LoginController.java
+++ project/src/main/java/br/lostpets/project/controller/LoginController.java
@@ -1,17 +1,21 @@
 package br.lostpets.project.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.lostpets.project.model.PetPerdido;
 import br.lostpets.project.model.Usuario;
+import br.lostpets.project.service.PetPerdidoService;
 import br.lostpets.project.service.SessionService;
 import br.lostpets.project.service.UsuarioService;
 import br.lostpets.project.utils.HistoricoAcessoLog;
@@ -20,45 +24,51 @@ import br.lostpets.project.utils.HistoricoAcessoLog;
 public class LoginController {
 
 	@Autowired
+	private PetPerdidoService petPerdidoService;
+	@Autowired
 	private UsuarioService usuarioService;
 	@Autowired
 	private HistoricoAcessoLog historicoAcessoLog;
 	@Autowired
 	private SessionService session;
-
+	
 	private Usuario usuario;
 	private ModelAndView modelAndView;
-
+	private boolean msn = false; 	
+	
 	@RequestMapping(value = { "/", "/LostPets" }, method = RequestMethod.GET)
-	public ModelAndView loginPage() {		
+	public String loginPage(Usuario usuario, Model model) {
+		
+		List<PetPerdido> pets;		
+		pets = petPerdidoService.encontrarPetsAtivos();
+		model.addAttribute("fotoAnimais", pets);
+		
 		if (session.existsSessionUsuario()) {
 			historicoAcessoLog.dataHora(usuario.getNome());
-			modelAndView.setViewName("redirect:/Dashboard");
-			return modelAndView;
+			return "redirect:/Dashboard";
+			
 		} else {
-			modelAndView = new ModelAndView();
-			usuario = new Usuario();
-			modelAndView.addObject("usuario", usuario);
-			modelAndView.setViewName("login");
-			return modelAndView;	
+			String mensagem;
+			mensagem = msn ? "<span class=\"COLOCAR MATERIALIZE\">Email ou senha Incorretos</span>": "";
+			
+			model.addAttribute("mensagem", mensagem);
+			return "login";	
 		}		
 	}
 
 	@PostMapping("/LostPets")
-	public ModelAndView logar(@Valid Usuario usuario, BindingResult bindingResult) {
-
+	public ModelAndView logar(@Valid Usuario usuario) {
+		
 		usuario = usuarioService.emailSenha(usuario.getEmail(), usuario.getSenha());
-		if (bindingResult.hasErrors()) {
-			modelAndView.setViewName("login");
-		} else if (usuario != null) {
+		if (usuario != null) {
 			modelAndView = new ModelAndView("redirect:/Dashboard");
 			historicoAcessoLog.dataHora(usuario.getNome());
 			session.setSessionUsuario(usuario);
+			
 		} else {
-			modelAndView.setViewName("login");
-			modelAndView.addObject("mensagem", "E-mail ou senha inválido");
+			modelAndView = new ModelAndView("redirect:/LostPets");
+			msn = true;
 		}
-
 		return modelAndView;
 	}
 
